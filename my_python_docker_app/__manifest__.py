9a9a9ee87225f3ec70896fbab5273566377fa79a
janmejaya swain <js@saaragh.com>
2024-11-18 13:37:52 +0530

    Update __manifest__.py

--- my_python_docker_app/__manifest__.py
+++ my_python_docker_app/__manifest__.py
@@ -3,9 +3,9 @@
     'name': 'My Python Docker App',
     'version': '1.0',
     'summary': 'A Python Docker app example for Odoo Marketplace.',
-    'author': 'Your Name',
-    'maintainer': 'Your Name/Company',
-    'website': 'https://www.yourwebsite.com',
+    'author': 'JS',
+    'maintainer': 'Navan',
+    'website': 'https://nchat.navan.ai',
     'category': 'Custom',
     'license': 'LGPL-3',
     'depends': ['base'],
@@ -13,6 +13,7 @@
         'views/my_docker_model_views.xml',
         'views/templates.xml',
     ],
+    'images': ['static/description/cover.png'],  # Added line for cover image
     'installable': True,
     'application': True,
 }
